ffbea0dc28fec7309860e79b609ebda3849a4997
girish-rajan <girishrajan2009@gmail.com>
2020-03-16 20:06:32 +0530

    Updates

---
+++ precache-manifest.a7fa6fddc6bfca468ea7976720654a22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187f634087a5f6fb86648cbd6c3a0f93",
+    "url": "/gir-robo/index.html"
+  },
+  {
+    "revision": "3dc8e89824fa47ea1a4d",
+    "url": "/gir-robo/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "2fc0bf39084b16e4875a",
+    "url": "/gir-robo/static/css/main.6881b41a.chunk.css"
+  },
+  {
+    "revision": "3dc8e89824fa47ea1a4d",
+    "url": "/gir-robo/static/js/2.58272216.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gir-robo/static/js/2.58272216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc0bf39084b16e4875a",
+    "url": "/gir-robo/static/js/main.2bd717f0.chunk.js"
+  },
+  {
+    "revision": "2fa401e61bc30c522a7d",
+    "url": "/gir-robo/static/js/runtime-main.942b30fd.js"
+  }
+]);
